90e0374ac4f54dcc65b21c9d82178a6dc0132ef7
Bryan Nishi <Bryan.Nishi@gmail.com>
2018-12-12 19:09:52 -0700

    basic html layouts and routing done. API Gets created.

---
+++ src/pages/Monster.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Monster = () => (
+    <div>
+        <h1>Monster Manual</h1>
+        <input>Search for Monster:</input>
+        <button>Search</button>
+        <a>Full Monster Manual/Search Result Loaded HERE</a>
+    </div>
+);
+
+export default Monster;
