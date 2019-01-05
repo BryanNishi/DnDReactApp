8b13ec2626884ed148888b58589774535e153396
Christian Rodier <crodier@protonmail.com>
2019-01-05 11:02:18 -0700

    added headers to monster search
    added headers to monsters search to see if API would work on heroku

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,43 +1,46 @@
 import axios from "axios";
 
 export default {
-  getMonster: function(monsters) {
-    return axios.get("http://dnd5eapi.co/api/monsters/" + monsters);
+  getMonster: function (monsters) {
+    return axios.get("http://dnd5eapi.co/api/monsters/" + monsters, {
+      dataType: "json",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" }
+    });
   },
-  getSpell: function(spells) {
+  getSpell: function (spells) {
     return axios.get("http://dnd5eapi.co/api/spells/" + spells);
   },
-  getClass: function(classes) {
+  getClass: function (classes) {
     return axios.get("http://dnd5eapi.co/api/classes/" + classes);
   },
-  getStartingEquipment: function(classIndex){
+  getStartingEquipment: function (classIndex) {
     return axios.get("http://www.dnd5eapi.co/api/startingequipment/" + classIndex)
   },
-  getfeature: function(features) {
+  getfeature: function (features) {
     return axios.get("http://dnd5eapi.co/api/features/" + features);
   },
-  getInn: function() {
+  getInn: function () {
     return axios.get("/api/inn");
   },
-  getLand: function() {
+  getLand: function () {
     return axios.get("/api/land");
   },
-  getDungeon: function() {
+  getDungeon: function () {
     return axios.get("/api/dungeon");
   },
-  getTreasure: function() {
+  getTreasure: function () {
     return axios.get("/api/treasure");
   },
-  getName: function() {
+  getName: function () {
     return axios.get("/api/name");
   },
-  getUser: function(){
+  getUser: function () {
     return axios.get("/api/user")
-  },  
-  postUser: function(data){
-  return axios.post(`api/user`,data);
- 
-  } 
+  },
+  postUser: function (data) {
+    return axios.post(`api/user`, data);
+
+  }
 
 };
 
