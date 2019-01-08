041d9de18e92c5c10f4e28cf711c72f09cccda9c
Christian Rodier <crodier@protonmail.com>
2019-01-07 17:13:32 -0700

    created NPC generator
    now npc generator works. put back in google  login.

--- controllers/GeneratorContoller.js
+++ controllers/GeneratorContoller.js
@@ -36,7 +36,7 @@ module.exports = {
       .find(req.query)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  },
+  },  
 
   //get user data
   getUsers: function(req, res) {
